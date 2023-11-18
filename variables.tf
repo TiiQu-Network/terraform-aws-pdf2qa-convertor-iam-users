181fd7795858ad24a5246cf846a2c417b14d286b
John O’Sullivan <osullivanj.01@gmail.com>
2023-11-18 18:46:35 +0700

    add user for backend-pdf2qa-validation-service deployment

--- variables.tf
+++ variables.tf
@@ -3,5 +3,6 @@ variable "users" {
   type        = map(string)
   default     = {
     "terraform-aws-pdf2qa-convertor-deploy-infrastructure" = "terraform-aws-pdf2qa-convertor-deploy-infrastructure-policy",
+    "backend-pdf2qa-validation-service-deploy" = "backend-pdf2qa-validation-service-deploy-policy",
   }
 }
